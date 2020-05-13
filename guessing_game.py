0639546c427c529d5fee900fc7f2383891b58d25
ahmadB <39441506+ahmadB01@users.noreply.github.com>
2020-05-13 11:45:27 +0200

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,147 +1,88 @@
 """
 Le script suivant est un programme du jeu du Plus ou Moins.
-Le but étant de choisir un nombre entre 0 et 10 000,
-et de le faire deviner à l'ordinateur en répondant à chaque tour
-"plus" ou "moins" pour guider l'ordinateur
-afin qu'il puisse déterminer le nombre.
-
-Le code utilise une approche fonctionnelle et déclarative.
+Le but étant de choisir un nombre dans un certain intervalle,
+et de le faire deviner à l'ordinateur en répondant à chaque
+tour par "plus" ou "moins" pour le guider afin qu'il puisse
+déterminer le nombre
 """
 
 # Constante utilisée pour récupérer les labels
-# que peut rentrer l'utilisateur pour guider l'ordinateur
+# de chaque commande pour guider l'ordinateur
 labels = {
     'high': ('plus', '+', 'higher', 'high'),
     'low': ('moins', '-', 'lower', 'low'),
     'eq': ('bravo', 'egal', '=', 'equal', 'eq'),
 }
 
-def str_label(cmd: [str]) -> str:
+def disp_labels(cmd):
     """
-    Fonction utilitaire qui permet de retourner le texte à afficher
-    pour présenter les différents labels,
-    pour que l'utilisateur guide l'ordinateur.
-
-    Elle prend en paramètre une list de chaines de caractères,
-    représentant les labels.
+    Fonction utilitaire qui permet de renvoyer la chaine
+    de caractères à afficher par commande
+    (cf. la constante `labels` et la fonction `ask`)
     """
     return ', ou '.join(
         map(lambda label: '"{}"'.format(label), cmd))
 
-def ask() -> str:
+def ask(guess, tries):
     """
-    Fonction qui permet de récupérer la saisie de l'utilisateur
-    sous forme de chaine de caractère, puis la retourne.
+    Fonction qui permet de récupérer
+    la réponse de l'utilisateur
     """
-    print('* Si le nombre auquel vous pensez est plus grand, entrez :\n  {}.'
-          .format(str_label(labels['high'])))
-    print('* Si le nombre auquel vous pensez est plus petit, entrez :\n  {}.'
-          .format(str_label(labels['low'])))
-    print('* Sinon, entrez :\n  {}.'
-          .format(str_label(labels['eq'])))
-    # On empêche l'utilisateur de rentrer une saisie incorrecte.
+    print('L\'ordinateur a deviné :', guess)
+    print('Coups :', tries, end='\n\n')
+    print('* Si le nombre auquel vous pensez est plus grand, entrez:\n {}.'
+          .format(disp_labels(labels['high'])))
+    print('* Si le nombre auquel vous pensez est plus petit, entrez:\n {}.'
+          .format(disp_labels(labels['low'])))
+    print('* Sinon, entrez:\n {}.'
+          .format(disp_labels(labels['eq'])))
+
+    # On empêche l'utilisateur
+    # de rentrer une valeur incorrecte
     while True:
-        # On retire les espaces et on met la saisie en minuscule,
-        # pour que la comparaison avec les labels reste valide.
         res = input('> ').strip().lower()
         if res in labels['high'] \
-                or res in labels['low'] \
-                or res in labels['eq']:
-            break
-    return res
+               or res in labels['low'] \
+               or res in labels['eq']:
+            return res
 
-def next_rng(rng: (int, int)) -> int:
+def update(res, rng):
     """
-    Fonction qui retourne le milieu de l'intervalle 
-    représenté par le tuple `rng`
-    passé en paramètre.
+    Fonction qui met à jour l'intervalle
+    en fonction de la réponse de l'utilisateur
     """
-    min, max = rng
-    return (min + max) // 2
-
-"""
-`higher` et `lower` sont deux fonctions
-qui prennent en paramètre
-un tuple qui représente l'intervalle de nombre
-dans lequel se trouve le nombre correct.
-"""
-def higher(rng: (int, int)) -> (int, int):
-    max = rng[1]
-    # Dans le cas où le nombre correct
-    # est supérieur au nombre deviné par l'ordinateur
-    # autrement dit `next_rng(rng)`,
-    # alors le nombre correct appartient à l'intervalle :
-    # ]next_rng(rng); max]
-    # où `max` représente le maximum de l'intervalle précédent.
-    return (next_rng(rng), max)
-
-def lower(rng: (int, int)) -> (int, int):
-    min = rng[0]
-    # Dans le cas où le nombre correct
-    # est inférieur à `next_rng(rng)`,
-    # alors le nombre correct appartient à l'intervalle :
-    # [min; next_rng(rng)[
-    # où `min` représente le minimum de l'intervalle précédent.
-    return (min, next_rng(rng))
-
-def update(res: str, rng: (int, int)) -> (int, int):
-    """
-    Fonction qui retourne un nouveau tuple
-    représentant l'intervalle de nombres
-    qui contient le nombre correct.
-    Le résultat dépend :
-    - de la réponse de l'utilisateur
-      entrée en paramètre (`res`) ;
-    - et de l'intervalle précédent (`rng`)
-      pour pouvoir en déduire le suivant.
-    """
+    guess = sum(rng) // 2
     if res in labels['high']:
-        return higher(rng)
+        return guess, rng[1]
     elif res in labels['low']:
-        return lower(rng)
+        return rng[0], guess
     else:
-        # Dans le cas où le nombre deviné par l'ordinateur
-        # est égal au nombre correct,
-        # alors on renvoit l'intervalle :
-        # [next_rng(rng); next_rng(rng)]
-        # de sorte à récupérer la valeur trouvée
-        # avec next_rng(rng) plus tard (dans la fonction game_loop).
-        return (next_rng(rng),) * 2
+        return guess, guess
 
-def game_loop(rng: (int, int), tries: [int]) -> int:
+def loop(rng, tries):
     """
-    Fonction qui renvoit le nombre correct final
-    trouvé par l'ordinateur.
-    
-    Elle prend en paramètre un tuple qui représente
-    l'intervalle de nombres qui contient
-    le nombre correct.
-    
-    C'est une fonction qui utilise la récursivité
-    pour former une boucle de jeu.
+    Fonction qui renvoit à la fin le nombre correct
+    trouvé par l'ordinateur
+    C'est une fonction récursive ce qui permet
+    de former une boucle de jeu
     """
     if rng[0] == rng[1]:
-        return next_rng(rng), len(tries)
+        return rng[0], len(tries)
     else:
-        guess = next_rng(rng)
-        tries_upt = tries + [guess]
-        print('L\'ordinateur a deviné :', guess)
-        print('Essais :', tries_upt)
-        res = ask()
-        return game_loop(update(res, rng), tries_upt)
+        guess = sum(rng) // 2
+        tries.append(guess)
+        res = ask(guess, tries)
+        return loop(update(res, rng), tries)
 
-def title(rng: (int, int)):
+def title(rng):
     """
-    Fonction qui permet d'afficher le texte
-    au début de la partie.
-    Elle dépend de `rng`, qui est le tuple
-    qui représente l'intervalle de nombre
-    qui contient le nombre correct.
+    Fonction utilitaire qui permet d'afficher le texte
+    au début de la partie
     """
-    min, max = rng
     print('Jeu du plus ou moins')
     print('====================')
-    print('Choisissez un nombre entre {} et {}'.format(min, max-1));
+    print('Choisissez un nombre entre {} et {}'
+          .format(rng[0], rng[1]-1));
     print('L\'ordinateur devra deviner le nombre auquel vous pensez')
     print('Notez donc bien ce nombre.')
     print('Appuyez sur Entrée pour commencer le jeu')
@@ -149,18 +90,14 @@ def title(rng: (int, int)):
 
 def main():
     """
-    Fonction principale,
-    qui fait donc office de point d'entrée.
+    Fonction qui fait office de point d'entrée
+    du programme
     """
-    # Le tuple initial qui représente l'intervalle de nombres
-    # qui contiendra le nombre correct.
-    # À chaque tour, sa taille diminuera pour donner
-    # une meilleure précision à l'ordinateur.
     init_rng = (0, 10001)
     title(init_rng)
-    result, n_tries = game_loop(init_rng, [])
+    result, n = loop(init_rng, [])
     print('L\'ordinateur a gagné encore une fois en {} coups!'
-          .format(n_tries))
+          .format(n))
     print('Le nombre auquel vous pensiez était donc :', result)
 
 if __name__ == '__main__':
